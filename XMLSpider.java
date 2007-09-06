a3e19fee2b5427726df68cf9f94b91450b837594
Matthew Toseland <toad@amphibian.dyndns.org>
2007-09-06 16:38:01 +0000

    imports
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/plugins/XMLSpider@14987 67a373e5-eb02-0410-a15c-ee090a768436

--- XMLSpider.java
+++ XMLSpider.java
@@ -7,7 +7,6 @@ import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.MalformedURLException;
