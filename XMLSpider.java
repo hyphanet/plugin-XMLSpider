85ae1a234ee5cfc660814960a46a187157850bd1
Daniel Cheng (鄭郁邦) <j16sdiz+freenet@gmail.com>
2008-12-29 16:45:16 +0000

    cache more
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/plugins/XMLSpider@24825 67a373e5-eb02-0410-a15c-ee090a768436

--- XMLSpider.java
+++ XMLSpider.java
@@ -641,6 +641,7 @@ public class XMLSpider implements FredPlugin, FredPluginHTTP, FredPluginThreadle
 	 */
 	private Storage initDB() {
 		Storage db = StorageFactory.getInstance().createStorage();
+		db.setProperty("perst.object.cache.init.size", 8192);
 		db.setProperty("perst.gc.threshold", 16384);
 		db.setProperty("perst.alternative.btree", true);
 		db.setProperty("perst.string.encoding", "UTF-8");
