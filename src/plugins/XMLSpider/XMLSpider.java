22339229a95e67b17fbe37ac29de4dcf9df58f86
Matthew Toseland <toad@amphibian.dyndns.org>
2010-05-07 17:46:00 +0100

    Increment version to 43 for build fix.

--- src/plugins/XMLSpider/XMLSpider.java
+++ src/plugins/XMLSpider/XMLSpider.java
@@ -87,7 +87,7 @@ public class XMLSpider implements FredPlugin, FredPluginThreadless,
 	protected Set<String> allowedMIMETypes;
 
 	static int dbVersion = 37;
-	static int version = 42;
+	static int version = 43;
 
 	public static final String pluginName = "XML spider " + version;
 
